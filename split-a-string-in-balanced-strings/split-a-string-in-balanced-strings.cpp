2a6d37519693023dc68d3464ea275a8d8dd4a603
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-05-20 22:00:33 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (9.76%) - LeetHub

--- split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
+++ split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -21,6 +21,17 @@ public:
     }
     
     int balancedStringSplit(string s) {
-    return solver(s,0,s.length()-1);
+    int ans=0;
+    int rc=0,lc=0;
+       
+    for(int i=0;i<=s.length();i++)
+    {
+        if(s[i]=='R') rc++;
+        else lc++;
+
+        if(rc==lc) 
+        { ans++; rc=lc=0;}
+    }
+    return ans;
     }
 };
