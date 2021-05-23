6da95ccd72a27e50e7aa445b8ac5a38af1a6df8f
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-05-23 12:14:30 +0530

    Time: 8 ms (97.32%), Space: 14.5 MB (8.78%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,83 +1,41 @@
 class Solution {
 public:
-    
-    int m,n;
-    map<pair<int,int>,int> mp;
-    pair<int,int> p;
-    bool solver(vector<vector<char>>& board, int i,int j, string &word, int index)
-    {   
-        p={i,j};
-        if( index>=word.length() or  word[index] != board[i][j] or mp[p]==1) 
-            return false;
-        
-        if(index==word.length()-1 && word[index]==board[i][j])
-            return true;
-      
-        if(i+1 < m) 
-            {   
-                pair<int,int> pp;
-                pp = {i,j};
-                mp[pp]=1;
-                if(solver(board,i+1,j,word,index+1) == true )
-                    return true; 
-                mp[pp]=0;
-            }
-        if(j+1 < n) 
-            {   
-                pair<int,int> pp;
-                pp = {i,j};
-                mp[pp]=1;
-                if(solver(board,i,j+1,word,index+1) == true )
-                    return true;     
-                mp[pp]=0;
-            }
-        if(i-1 >=0 ) 
-            {   
-                pair<int,int> pp;
-                pp = {i,j};
-                mp[pp]=1;
-                if(solver(board,i-1,j,word,index+1) == true )
-                   return true; 
-                 mp[pp]=0;
-            }
-         if(j-1 >=0 ) 
-            {   
-                pair<int,int> pp;
-                pp = {i,j};
-                mp[pp]=1;
-                if(solver(board,i,j-1,word,index+1) == true )
-                    return true; 
-                 mp[pp]=0;
-            }
+    bool dfs(int i, int j, int idx, vector<vector<char>> &board, string word, vector<vector<int>> used){
+        if(idx == word.length()) return true;
+        if(i>=board.size() || j>=board[0].size() || i<0 || j<0 || used[i][j]) return false;
+        else if(board[i][j] == word[idx]){
+            used[i][j] = 1;
+            bool ans = false;
+            ans = ans || dfs(i-1, j, idx+1, board, word, used);
+            if(ans) return true;
+            ans = ans || dfs(i+1, j, idx+1, board, word, used);
+            if(ans) return true;
+            ans = ans || dfs(i, j-1, idx+1, board, word, used);
+            if(ans) return true;
+            ans = ans || dfs(i, j+1, idx+1, board, word, used);
+            return ans;
+        }
         return false;
     }
     
-    
-    void makeboard()
-    {
-        for(int x=0;x<m;x++)
-             for(int y=0;y<n;y++)
-             {
-                 p={x,y};
-                  mp[p]=0;
-             }
-    }
-    
-    
-    bool exist(vector<vector<char>>& board, string word) 
-    {
-         m= board.size();
-         n=board[0].size();
-         makeboard();
-         for(int i=0;i<m;i++)
-         {
-             for(int j=0;j<n;j++)
-             {  
-                 
-                 if(solver(board,i,j,word,0))
-                     return true;
-             }
-         }
-        return false;
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>> used(n, vector<int>(m, 0));
+        int wordlen = word.length();
+        if(wordlen > n*m) return false;
+        bool ans = false;
+        for(int i=0;i < word.length();i++){
+            char cur = word[i];
+            bool found = false;
+            for(int r=0;r < n;r++) for(int c=0; c < m; c++) if(board[r][c] == cur) found=true;
+            if(found==false) return false;
+        }
+        for(int i=0;i < n;i++){
+            for(int j=0;j < m;j++){
+                if(dfs(i, j, 0, board, word, used)) return true;
+            }
+        }
+        return ans;
     }
 };
