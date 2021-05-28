5bb37984d7a80e752ee45a38ad44fb0c045c9605
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-05-28 16:21:21 +0530

    Time: 12 ms (99.85%), Space: 10.6 MB (76.95%) - LeetHub

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    Solution() { ios_base::sync_with_stdio(0); cin.tie(NULL); }
+    string minRemoveToMakeValid(string s) 
+    {
+      stack<int> st;
+      for (auto i = 0; i < s.size(); ++i)
+          {
+            if (s[i] == '(') st.push(i);
+            if (s[i] == ')') {
+              if (!st.empty()) st.pop();
+              else s[i] = '*';
+            }
+          }
+      while (!st.empty()) 
+          {
+            s[st.top()] = '*';
+            st.pop();
+          } 
+      s.erase(remove(s.begin(), s.end(), '*'), s.end());
+      return s;  
+    }
+};
