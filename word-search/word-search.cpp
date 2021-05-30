961bb9b36f0c11e7e3b2589813560358331e391f
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-05-30 23:47:23 +0530

    Time: 396 ms (20.17%), Space: 7.5 MB (8.23%) - LeetHub

--- word-search/word-search.cpp
+++ word-search/word-search.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-    bool dfs(int i, int j, int idx, vector<vector<char>> &board, string word, vector<vector<int>> used){
-        if(idx == word.length()) return true;
-        if(i>=board.size() || j>=board[0].size() || i<0 || j<0 || used[i][j]) return false;
-        else if(board[i][j] == word[idx]){
-            used[i][j] = 1;
-            bool ans = false;
-            ans = ans || dfs(i-1, j, idx+1, board, word, used);
-            if(ans) return true;
-            ans = ans || dfs(i+1, j, idx+1, board, word, used);
-            if(ans) return true;
-            ans = ans || dfs(i, j-1, idx+1, board, word, used);
-            if(ans) return true;
-            ans = ans || dfs(i, j+1, idx+1, board, word, used);
-            return ans;
-        }
-        return false;
-    }
-    
+    bool findWord(vector<vector<char>> &Board,int m, int n,int r, int c, string Word, int len, int idx){
+        if(idx == len) return true;
+        if(r<0 || c<0 || r>=m || c >=n) return false;
+        if(Board[r][c]!= Word[idx]) return false;
+        char gg =Board[r][c];
+        Board[r][c] = '#';
+        bool res= findWord(Board,m,n,r+1,c,Word,len,idx+1)  
+            ||  findWord(Board,m,n,r,c+1,Word,len,idx+1) 
+            || findWord(Board,m,n,r-1,c,Word,len,idx+1)
+            ||  findWord(Board,m,n,r,c-1,Word,len,idx+1);
+        Board[r][c] = gg;
+        return res;
+        
+    } 
+        
     bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        vector<vector<int>> used(n, vector<int>(m, 0));
-        int wordlen = word.length();
-        if(wordlen > n*m) return false;
-        bool ans = false;
-        for(int i=0;i < word.length();i++){
-            char cur = word[i];
-            bool found = false;
-            for(int r=0;r < n;r++) for(int c=0; c < m; c++) if(board[r][c] == cur) found=true;
-            if(found==false) return false;
-        }
-        for(int i=0;i < n;i++){
-            for(int j=0;j < m;j++){
-                if(dfs(i, j, 0, board, word, used)) return true;
+        int m = board.size();
+        if(m==0) return false;
+        int n = board[0].size();
+        int len = word.length();
+        
+        for(int r=0; r<m; ++r)
+            for(int c=0; c<n; ++c){
+                vector<vector<bool>> visited(m,vector<bool>(n));        
+                if(findWord(board,m,n,r,c,word,len,0)) 
+                    return true;
             }
-        }
-        return ans;
+        return false;
     }
 };
