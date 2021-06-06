c9c1fee27f05d720dfea36d1ed03c49ef02d7b36
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-06-07 00:51:04 +0530

    Time: 8 ms (51.32%), Space: 7.3 MB (31.55%) - LeetHub

--- regular-expression-matching/regular-expression-matching.cpp
+++ regular-expression-matching/regular-expression-matching.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
-    
-    bool solver(string &s,string &p,int a ,int b)
-    {
+    bool solver(string &s,string &p,int a ,int b,vector<vector<int>> &dp)
+    {   
+        if(dp[a][b]!=-1)
+            return dp[a][b];
         if(a>=s.length() and b>=p.length()) 
             return true;
         if(b>=p.length())
@@ -10,14 +11,15 @@ public:
         bool match = ( a <s.length() and (s[a]==p[b] or p[b]=='.'));
         
         if( b<p.length() and p[b+1]=='*')
-            return solver(s,p,a,b+2) or ( match and solver(s,p,a+1,b));
+            return dp[a][b] = solver(s,p,a,b+2,dp) or ( match and solver(s,p,a+1,b,dp));
         if(match)
-            return solver(s,p,a+1,b+1);
+            return dp[a][b] = solver(s,p,a+1,b+1,dp);
         return false;
     }
     
     
     bool isMatch(string s, string p) {
-    return solver(s,p,0,0);
+    vector<vector<int>> dp( s.length()+1, vector<int>(p.length()+1,-1) );
+    return solver(s,p,0,0,dp);
     }
 };
