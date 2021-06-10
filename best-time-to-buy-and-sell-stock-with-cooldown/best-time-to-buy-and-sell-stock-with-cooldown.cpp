fc9f5204c4e3581faf6f5cd3eaaa601dd2a8c8f7
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-06-10 14:08:57 +0530

    Time: 828 ms (5.37%), Space: 55.6 MB (5.10%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    int dp[5001][1001][2];
+public:
+    int solver(vector<int> &prices , int index , int bs , int cp)
+    {
+        if(index >= prices.size() ) return 0;
+        if( dp[index][cp][bs] != -1)
+            return dp[index][cp][bs];
+        if(bs==0)
+            return dp[index][cp][bs] = max( solver(prices , index+1 , 0 , cp) , solver(prices , index+1 , 1, prices[index]));
+        
+        return dp[index][cp][bs] = max( solver(prices , index+1 , 1 , cp) , ( prices[index] - cp) + solver(prices , index+2, 0 , 0) );
+    
+    }
+    
+    int maxProfit(vector<int>& prices) {
+    memset( dp ,-1 , sizeof(dp));
+    return solver(prices , 0 , 0 , 0);
+    }
+};
