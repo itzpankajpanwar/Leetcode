c16a478b3cdb1ee83c8b5344f2049470e80bded5
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-06-10 16:39:07 +0530

    Time: 704 ms (61.45%), Space: 135.4 MB (64.98%) - LeetHub

---
+++ arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,27 @@
+#define LL long long
+class Solution {
+public:
+   Solution() { ios_base::sync_with_stdio(0); cin.tie(NULL); }
+    
+    
+   int numberOfArithmeticSlices(vector<int>& A)
+    {
+    int n = A.size();
+        LL ans = 0;
+        vector<map<LL, int>> cnt(n);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                LL delta = (LL)A[i] - (LL)A[j];
+                int sum = 0;
+                if (cnt[j].find(delta) != cnt[j].end()) {
+                    sum = cnt[j][delta];
+                }
+                cnt[i][delta] += sum + 1;
+                ans += sum;
+            }
+        }
+
+        return (int)ans;
+    
+    }
+};
