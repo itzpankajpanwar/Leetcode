d4ec9611395a62a0611761c889f5adff745b505f
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-06-10 14:18:52 +0530

    Time: 836 ms (5.37%), Space: 55.7 MB (5.10%) - LeetHub

--- best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,20 +1,21 @@
 class Solution {
     int dp[5001][1001][2];
 public:
-    int solver(vector<int> &prices , int index , int bs , int cp)
+    Solution() { ios_base::sync_with_stdio(0); cin.tie(NULL); }
+    int solver(vector<int> &prices , int index , int bs , int cp )
     {
         if(index >= prices.size() ) return 0;
         if( dp[index][cp][bs] != -1)
             return dp[index][cp][bs];
         if(bs==0)
-            return dp[index][cp][bs] = max( solver(prices , index+1 , 0 , cp) , solver(prices , index+1 , 1, prices[index]));
+            return dp[index][cp][bs] = max( solver(prices , index+1 , 0 , cp ) , solver(prices , index+1 , 1, prices[index]));
         
-        return dp[index][cp][bs] = max( solver(prices , index+1 , 1 , cp) , ( prices[index] - cp) + solver(prices , index+2, 0 , 0) );
+        return dp[index][cp][bs] = max( solver(prices , index+1 , 1 , cp ) , ( prices[index] - cp) + solver(prices , index+2, 0 , 0 ) );
     
     }
     
     int maxProfit(vector<int>& prices) {
-    memset( dp ,-1 , sizeof(dp));
-    return solver(prices , 0 , 0 , 0);
+    memset(dp,-1,sizeof(dp));
+    return solver(prices , 0 , 0 , 0 );
     }
 };
