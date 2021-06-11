9fc25e9e2a4f050626e243b94306472abc8e9016
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-06-11 16:39:35 +0530

    Time: 20 ms (93.17%), Space: 7.6 MB (54.17%) - LeetHub

--- minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    int minInsertions(string s) {
-        
+    Solution() { ios_base::sync_with_stdio(0); cin.tie(NULL); }
+    int minInsertions(string s) { 
     int n=s.length();
     int dp[n+1][n+1];
     memset( dp ,0 , sizeof(dp));
-    
     for(int i=1;i<n+1;i++)
         for(int j=1; j <n+1 ; j++)  
         dp[i][j] = (s[i-1] == s[(n-1)-(j-1)] ) ? 1+dp[i-1][j-1] : max(dp[i-1][j] , dp[i][j-1]);
