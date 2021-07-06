8102e5900ae173da2fe136492e4652a1c12b651c
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-07-06 14:28:58 +0530

    Time: 68 ms (55.82%), Space: 47.3 MB (13.74%) - LeetHub

---
+++ super-egg-drop/super-egg-drop.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+Solution(){ ios_base::sync_with_stdio(0); cin.tie(NULL); }
+    int superEggDrop(int K, int N) 
+{
+        vector<vector<int>> dp(N + 1, vector<int>(K + 1, 0));
+        int m = 0;
+        while (dp[m][K] < N) {
+            m++;
+            for (int k = 1; k <= K; ++k)
+                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
+        }
+        return m;
+    }
+};
+
