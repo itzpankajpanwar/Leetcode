1ea6a188c1ceaada22dd761dc3b04a7d921d8fc7
itzpankajpanwar <44938486+itzpankajpanwar@users.noreply.github.com>
2021-07-16 15:37:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (79.11%) - LeetHub

---
+++ check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n)
+    {
+     while (n > 1 && n % 3 != 2) {
+            if (n % 3 == 0) {
+                n /= 3;
+            }else {
+                --n;
+            }
+        }
+        return n == 1;
+    }
+};
